d675626c82dab9be63c9940e747201254e2ec2e0
Alyson Takai <shtakai@gmail.com>
2016-05-24 10:38:10 -0700

    finished assignment question and choices

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Quiz(models.Model):
+    created_at    = models.DateField(auto_now=True)
+    updated_at    = models.DateField(auto_now=True)
+    question_text = models.CharField(max_length=255)
+    pud_date      = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'quizzes'
+
+
+class Choice(models.Model):
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now=True)
+    quiz       = models.ForeignKey(Quiz)
+
+    class Meta:
+        db_table = 'choices'
+
